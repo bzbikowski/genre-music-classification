15dc552b49a4bbc557f5dcb334c7f42d1e89c2d3
Bartosz Żbikowski <zbiku112@gmail.com>
2019-05-17 12:15:40 +0200

    Created utils folder, updated README file

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
 # Genre music classification
 
-Classification of the music tracks to different music genres.
+### Classification of the music tracks to different music genres.
 
-### Description
+#### Description
 
-Work in progress...
+The purpose of this application is to train and test the model of classifier of music based on its genre.
+Preparing and downloading dataset are automatically executed by the algorithm.
 
-### Database:
+To start training, run this commands:
+```
+pip install -r requirements.txt
+python train.py
+```
+
+After model is created, to test model run command:
+```
+python test.py
+```
+
+To create confusion matrix:
+```
+cd utils
+python plot_matrix.py
+```
+Confusion matrix can be found under `conf_matrix.png`.
+
+#### Database:
 
 GTZAN - 1000 tracks - 30 seconds - 10 genres, 100 tracks each
 
-### Links:
+#### Links:
 
 Database - http://opihi.cs.uvic.ca/sound/genres.tar.gz
--- utils/plot_matrix.py
+++ utils/plot_matrix.py
@@ -1,16 +1,16 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == "__main__":
     labels = ["jazz", "blues", "reggae", "pop", "disco", "country", "metal", "hip-hop", "rock", "classical"]
-    matrix = np.loadtxt("./model/matrix.txt", dtype='int16')
+    matrix = np.loadtxt("../model/matrix.txt", dtype='int16')
 
     colors = plt.cm.get_cmap("coolwarm")
 
     row_colors = colors(np.linspace(0.25, 0.5, len(labels)))
 
-    cell_colors = [[colors(i) for _ in np.linspace(0.25, 0.5, len(labels))] for i in np.linspace(0.25, 0.5, len(labels))]
+    cell_colors = [[colors(i) for _ in np.linspace(0.25, 0.5, len(labels))]
+                   for i in np.linspace(0.25, 0.5, len(labels))]
 
     fig, ax = plt.subplots()
 
@@ -26,6 +26,5 @@ if __name__ == "__main__":
              colLabels=labels,
              loc='center')
     fig.tight_layout()
-    plt.savefig("test.png")
+    plt.savefig("conf_matrix.png")
     plt.show()
-
